61cd0f8f479065cb37bf7d6fda3e62de47bde03a
garmoska89 <sergiuplam@gmail.com>
2015-02-08 20:34:44 +0200

    interval comment for commontypes.h

--- tata/library/commontypes.h
+++ tata/library/commontypes.h
@@ -8,13 +8,13 @@
 #ifndef COMMONTYPES_H_
 #define COMMONTYPES_H_
 
-typedef unsigned char                  	uint8;
-typedef char						  	int8;
-typedef unsigned short                 	uint16;
-typedef short                 			int16;
-typedef unsigned int                   	uint32;
-typedef int                   			int32;
+typedef unsigned char                  	uint8;	//-128 - 127
+typedef char						  	int8;	//0-255
+typedef unsigned short                 	uint16;	//0-65535
+typedef short                 			int16;	//-32768 - 32767
+typedef unsigned int                   	uint32;	//0 - 4 294 967 295
+typedef int                   			int32;	//-2147483648 - 2147483647
 typedef unsigned long long int         	uint64;
-typedef long long int         			int64;
+typedef long long int         			int64;	//-9223372036854775808 - 9223372036854775807
 
 #endif /* COMMONTYPES_H_ */
